e4ed60f78b542507de99e00aafa014e493de61cd
blaine <blaine.xu@kaiostech.com>
2020-04-03 18:27:34 +0800

    practice

---
+++ parameter_passing.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# parameter passing study
+# 2020-04-03 blaine xu
+echo "parameter pass"
+echo "shell 传递参数实例"
+echo "执行的文件名：$0"
+echo "执行的文件名：$1"
+echo "执行的文件名：$2"
+echo "执行的文件名：$3"
+# to be continued
